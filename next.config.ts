3466fef1db9d3a04aa223b9e8e7787b25be18522
shamimhcp1 <shamimhcp1@gmail.com>
2025-09-02 00:30:25 +0600

    nextjs config updatd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["https://fb-messenger.loca.lt"],
 };
 
 export default nextConfig;
